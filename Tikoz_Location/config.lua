837a0c278b05fbf8b5214080847415ed8897042f
Tikoz <106456046+Tikozaal@users.noreply.github.com>
2022-07-10 14:50:16 +0200

    Add files via upload

---
+++ Tikoz_Location/config.lua
@@ -0,0 +1,27 @@
+Config = {}
+
+Config.Pos = {
+    
+    Ped = vector4(-1037.15, -2730.65, 20.17, 149.85),
+
+    Menu = vector3(-1037.86, -2731.59, 19.17),
+
+    Spawn = vector4(-1076.8, -2581.31, 20.07, 152.56)
+
+}
+
+Config.Name = {
+
+    Car = "blista",
+
+    Scooter = "Faggio"
+
+}
+
+Config.Cross = {
+
+    Pnj = vector4(1525.75, -3272.2, 28.41, 184.15),
+
+    Menu = vector3(1525.73, -3273.25, 27.41)
+
+}
